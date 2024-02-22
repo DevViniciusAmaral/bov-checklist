295b4d03cdd6ac27611099970ef9ec4b874a0298
DevViniciusAmaral <viniciusamaraldev@gmail.com>
2024-02-22 18:50:28 -0300

    feat: adicionado imagens

---
+++ src/application/assets/images/index.ts
@@ -0,0 +1,7 @@
+export const images = {
+  checklist: require("./checklist.png"),
+  farmer: require("./farmer.png"),
+  map: require("./map.png"),
+  milk: require("./milk.png"),
+  supervisor: require("./supervisor.png"),
+};
